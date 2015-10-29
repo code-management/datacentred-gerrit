1282de7f61136fd2d4ed7e2ae5f7061fa45ba9c7
David Hayes <evidex@freeshell.de>
2015-10-29 16:08:29 +0000

    Fix gerrit::jar permissions for both Wget & S3 bucket method

--- manifests/install/app.pp
+++ manifests/install/app.pp
@@ -6,23 +6,26 @@ class gerrit::install::app {
 
   include ::gerrit
 
+  exec { "fix_war_perms":
+    command => "chown ${gerrit::user}:${gerrit::user} $gerrit::jar && chmod 774 $gerrit::jar",
+    path => '/bin',
+  }
+
   if $gerrit::s3_bucket {
     # Download WAR file from S3 Bucket
     # Expects AWS CLI tools, and permissions to be already configured
     exec { "aws s3 cp s3://${gerrit::s3_bucket}/gerrit-${gerrit::version}.war $gerrit::jar": 
       path => '/usr/local/bin',
       logoutput => true,
-    } -> 
-    exec { "chown ${gerrit::user}:${gerrit::user} $gerrit::jar && chmod 774 $gerrit::jar":
-      path => '/bin'
-    }
-    
+      before => Exec['fix_war_perms'],
+    }     
+
   } else {
     # Download WAR file using wget
     wget::fetch { "${gerrit::war_url}":
       destination => $gerrit::jar,
+      before => Exec['fix_war_perms'],
     }
-  }
-
+  }  
 
 }
