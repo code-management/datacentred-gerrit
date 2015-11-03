d7754f99afe49e204c60d4525c9ef70342110917
David Hayes <evidex@freeshell.de>
2015-11-03 13:38:36 +0000

    Identation fixes

--- manifests/install/app.pp
+++ manifests/install/app.pp
@@ -8,24 +8,24 @@ class gerrit::install::app {
 
   exec { "fix_war_perms":
     command => "chown ${gerrit::user}:${gerrit::user} $gerrit::jar && chmod 774 $gerrit::jar",
-    path => '/bin',
+    path    => '/bin',
   }
 
   if $gerrit::s3_bucket {
     # Download WAR file from S3 Bucket
     # Expects AWS CLI tools, and permissions to be already configured
     exec { "aws s3 cp s3://${gerrit::s3_bucket}/gerrit-${gerrit::version}.war $gerrit::jar": 
-      path => '/usr/local/bin',
+      path      => '/usr/local/bin',
       logoutput => true,
-      before => Exec['fix_war_perms'],
-      creates => $gerrit::jar,
+      before    => Exec['fix_war_perms'],
+      creates   => $gerrit::jar,
     }     
 
   } else {
     # Download WAR file using wget
     wget::fetch { "${gerrit::war_url}":
       destination => $gerrit::jar,
-      before => Exec['fix_war_perms'],
+      before      => Exec['fix_war_perms'],
     }
   }  
 
